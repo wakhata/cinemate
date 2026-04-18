185992b3e3f74ccc03f7e21c629a831eac4aed1a
wakhata <hatah01@gmail.com>
2026-04-18 13:13:22 +0300

    Updated warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,16 @@ export const Footer = () => {
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2026 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.</span>
             <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
                 </li>
                 <li>
-                    <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">LinkedIn</a>
                 </li>
                 <li>
-                    <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Youtube</a>
+                    <a href="https://www.youtube.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Youtube</a>
                 </li>
                 <li>
-                    <a href="#" target="_blank" className="hover:underline">Github</a>
+                    <a href="https://github.com" target="_blank" rel="noreferrer" className="hover:underline">Github</a>
                 </li>
             </ul>
         </footer>
