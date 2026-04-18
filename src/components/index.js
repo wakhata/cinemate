d227c3d4597a8bc291eb767732e9cb37fbf840c9
wakhata <hatah01@gmail.com>
2026-04-18 12:23:40 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { Footer } from './Footer';
+export { Header } from './Header';
+
+export { Card } from './Card';
+
+export { Button } from './Button';
+export { ScrollToTop } from './ScrollToTop';
---
+++ src/hooks/index.js
@@ -0,0 +1,2 @@
+export { useTitle } from './useTitle';
+export { useFetch } from './useFetch';
